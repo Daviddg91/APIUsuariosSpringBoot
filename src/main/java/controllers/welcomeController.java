434740818d1149950ef5475af8d2aca6b1463568
David <46385259+Daviddg91@users.noreply.github.com>
2020-02-28 02:23:23 +0100

    modified:   pom.xml 	modified:   src/main/java/controllers/welcomeController.java 	modified:   src/main/resources/application.properties 	new file:   src/main/resources/static/css/core.css 	new file:   src/main/resources/templates/fragments/alert.html 	new file:   src/main/resources/templates/fragments/footer.html 	new file:   src/main/resources/templates/fragments/header.html 	new file:   src/main/resources/templates/index.html 	new file:   src/main/resources/templates/layouts/layout.html 	new file:   src/main/resources/templates/text/text-template.txt 	new file:   src/main/resources/templates/th-form.html 	new file:   src/main/resources/templates/th-objects.html 	deleted:    src/main/webapp/WEB-INF/pages/welcome.jsp

--- src/main/java/controllers/welcomeController.java
+++ src/main/java/controllers/welcomeController.java
@@ -1,14 +1,19 @@
 package controllers;
 
+import java.time.LocalDateTime;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class welcomeController {
+class HomeController {
 
-   @RequestMapping("/welcome")
-   public String handler (   ) {
-      
-       return "welcome";
-   }
+    @GetMapping("/welcome")
+    String index(Model model) {
+        model.addAttribute("now", LocalDateTime.now());
+        return "index";
+    }
+    
 }
